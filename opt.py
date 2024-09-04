c621eb79c5e018060977bf14e0ed395f59cc6672
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2024-09-04 08:49:34 +0200

    Format

--- opt.py
+++ opt.py
@@ -2,13 +2,19 @@ from scipy.optimize import differential_evolution, dual_annealing, minimize
 
 
 def optimization_sa(mytarget, initial_guess, bounds, *args):
-
     def callback_print(params, f, context):
         func_value = mytarget(params, *args)
         print("Function value:", func_value)
         # print("Parameters:", params)
 
-    result = dual_annealing(mytarget, bounds=bounds, args=args, callback=callback_print, seed=40, x0=initial_guess)
+    result = dual_annealing(
+        mytarget,
+        bounds=bounds,
+        args=args,
+        callback=callback_print,
+        seed=40,
+        x0=initial_guess,
+    )
 
     print("Optimization result:")
     print("  Success:", result.success)
@@ -27,7 +33,16 @@ def optimization_nelder(mytarget, initial_guess, bounds, *args):
         print("Function value:", func_value)
         # print("Parameters:", params)
 
-    result = minimize(mytarget, initial_guess, args=args, bounds=bounds, method="Nelder-Mead", tol=1e-7, callback=callback_print, options={'maxiter': 100000})
+    result = minimize(
+        mytarget,
+        initial_guess,
+        args=args,
+        bounds=bounds,
+        method="Nelder-Mead",
+        tol=1e-7,
+        callback=callback_print,
+        options={"maxiter": 100000},
+    )
     print("Optimization result:", result.message)
     print("Optimization result:")
     print("  Message:", result.message)
@@ -46,7 +61,15 @@ def optimization_bfgs(mytarget, initial_guess, bounds, *args):
         print("Function value:", func_value)
         # print("Parameters:", params)
 
-    result = minimize(mytarget, initial_guess, args=args, bounds=bounds, method="L-BFGS-B", tol=1e-7, callback=callback_print)
+    result = minimize(
+        mytarget,
+        initial_guess,
+        args=args,
+        bounds=bounds,
+        method="L-BFGS-B",
+        tol=1e-7,
+        callback=callback_print,
+    )
     print("Optimization result:", result.message)
     print("Optimization result:")
     print("  Message:", result.message)
@@ -60,12 +83,13 @@ def optimization_bfgs(mytarget, initial_guess, bounds, *args):
 
 
 def optimization_DE(mytarget, initial_guess, bounds, *args):
-
     def callback_print(xk, convergence):
         print("Objective function values:", mytarget(xk, *args))
         # You can add more details about the optimization process here
 
-    result = differential_evolution(mytarget, bounds, args=args, popsize=30, callback=callback_print)
+    result = differential_evolution(
+        mytarget, bounds, args=args, popsize=30, callback=callback_print
+    )
 
     print("Optimization result:")
     print("  Message:", result.message)
--- pot.py
+++ pot.py
@@ -1,11 +1,14 @@
 import random
+from itertools import combinations_with_replacement
 
 from ase.calculators.morse import MorsePotential
 
 
 def Morse_field(parameters):
     epsilon, rho0, r0, rcut1, rcut2 = parameters
-    Potential = MorsePotential(epsilon=epsilon, rho0=rho0, r0=r0, rcut1=rcut1, rcut2=rcut2)
+    Potential = MorsePotential(
+        epsilon=epsilon, rho0=rho0, r0=r0, rcut1=rcut1, rcut2=rcut2
+    )
     return Potential
 
 
@@ -13,9 +16,6 @@ def generate_random_numbers(n, lower_limit, upper_limit, seed):
     random.seed(seed)
     random_numbers = [random.uniform(lower_limit, upper_limit) for _ in range(n)]
     return random_numbers
-
-
-from itertools import combinations_with_replacement
 
 
 def read_untrained_MTP(file_path):
@@ -26,7 +26,7 @@ def read_untrained_MTP(file_path):
     # Convert the text to YAML format
     yaml_data = {}
     for line in lines:
-        d = line.strip().split(' = ')
+        d = line.strip().split(" = ")
         if len(d) > 1:
             key, value = d
             yaml_data[key] = value
@@ -43,22 +43,32 @@ def brac(input):
         return "{{{}}}".format(",".join(map(str, input)))
 
 
-def write_MTP(file, species_count, min_dist, max_dist, scaling, radial_coeffs, species_coeffs, moment_coeffs, yaml_data):
-    version = yaml_data['version']
-    potential_name = yaml_data['potential_name']
-    potential_tag = ''
-    radial_basis_type = yaml_data['radial_basis_type']
-    radial_basis_size = yaml_data['radial_basis_size']
-    radial_funcs_count = yaml_data['radial_funcs_count']
-    alpha_moments_count = yaml_data['alpha_moments_count']
-    alpha_index_basic_count = yaml_data['alpha_index_basic_count']
-    alpha_index_basic = yaml_data['alpha_index_basic']
-    alpha_index_times_count = yaml_data['alpha_index_times_count']
-    alpha_index_times = yaml_data['alpha_index_times']
-    alpha_scalar_moments = yaml_data['alpha_scalar_moments']
-    alpha_moment_mapping = yaml_data['alpha_moment_mapping']
+def write_MTP(
+    file,
+    species_count,
+    min_dist,
+    max_dist,
+    scaling,
+    radial_coeffs,
+    species_coeffs,
+    moment_coeffs,
+    yaml_data,
+):
+    version = yaml_data["version"]
+    potential_name = yaml_data["potential_name"]
+    potential_tag = ""
+    radial_basis_type = yaml_data["radial_basis_type"]
+    radial_basis_size = yaml_data["radial_basis_size"]
+    radial_funcs_count = yaml_data["radial_funcs_count"]
+    alpha_moments_count = yaml_data["alpha_moments_count"]
+    alpha_index_basic_count = yaml_data["alpha_index_basic_count"]
+    alpha_index_basic = yaml_data["alpha_index_basic"]
+    alpha_index_times_count = yaml_data["alpha_index_times_count"]
+    alpha_index_times = yaml_data["alpha_index_times"]
+    alpha_scalar_moments = yaml_data["alpha_scalar_moments"]
+    alpha_moment_mapping = yaml_data["alpha_moment_mapping"]
 
-    with open(file, 'w') as f:
+    with open(file, "w") as f:
         f.write("MTP\n")
         f.write("version = {}\n".format(version))
         f.write("potential_name = {}\n".format(potential_name))
