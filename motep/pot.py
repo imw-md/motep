4c16902887e1eed60294340c6f0193f1e5e5071f
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2024-09-04 18:23:01 +0200

    Store numerical values in `int` and `float`

--- motep/pot.py
+++ motep/pot.py
@@ -1,6 +1,6 @@
 import random
 from itertools import combinations_with_replacement
-from typing import Any
+from typing import Any, TextIO
 
 from ase.calculators.morse import MorsePotential
 
@@ -19,32 +19,62 @@ def generate_random_numbers(n, lower_limit, upper_limit, seed):
     return random_numbers
 
 
-def read_untrained_MTP(file_path) -> dict[str, Any]:
-    with open(file_path, "r", encoding="utf-8") as file:
-        lines = file.readlines()
+def _parse_radial_coeffs(
+    file: TextIO,
+    data: dict[str, Any],
+) -> dict[tuple[int, int], list[list[float]]]:
+    d = {}
+    for _ in range(data["species_count"]):
+        for _ in range(data["species_count"]):
+            key = tuple(int(_) for _ in next(file).strip().split("-"))
+            value = []
+            for _ in range(data["radial_funcs_count"]):
+                tmp = next(file).strip().strip("{}").split(",")
+                value.append([float(_) for _ in tmp])
+            d[key] = value
+    return d
 
+
+def read_untrained_MTP(file_path) -> dict[str, Any]:
     data = {}
-    for line in lines:
-        d = line.strip().split(" = ")
-        if len(d) > 1:
-            key, value = d
-            if value.isdigit():
-                data[key] = int(value)
-            elif key in ["scaling", "min_dist", "max_dist"]:
-                data[key] = float(value)
-            else:
-                data[key] = value
-        else:
-            data[d[0]] = None
+    with open(file_path, "r", encoding="utf-8") as file:
+        for line in file:
+            if line.strip() == "MTP":
+                continue
+            if "=" in line:
+                key, value = [_.strip() for _ in line.strip().split("=")]
+                if key in ["scaling", "min_dist", "max_dist"]:
+                    data[key] = float(value)
+                elif value.isdigit():
+                    data[key] = int(value)
+                elif key in ["alpha_moment_mapping"]:
+                    data[key] = [int(_) for _ in value.strip("{}").split(",")]
+                elif key in ["species_coeffs", "moment_coeffs"]:
+                    data[key] = [float(_) for _ in value.strip().strip("{}").split(",")]
+                elif key in ["alpha_index_basic", "alpha_index_times"]:
+                    data[key] = [
+                        [int(_) for _ in _.split(",")]
+                        for _ in value.strip("{}").split("}, {")
+                        if _ != ""
+                    ]
+                else:
+                    data[key] = value.strip()
+            elif line.strip() == "radial_coeffs":
+                key = "radial_coeffs"
+                data[key] = _parse_radial_coeffs(file, data)
 
     return data
 
 
-def brac(input):
-    if len(input) == 1:
-        return "{{{}}}".format(input[0])
-    else:
-        return "{{{}}}".format(",".join(map(str, input)))
+def brac(value: list) -> str:
+    if len(value) == 0:
+        return "{}"
+    if isinstance(value[0], float):
+        return "{" + ", ".join(f"{_:21.15e}" for _ in value) + "}"
+    if isinstance(value[0], int):
+        return "{" + ", ".join(f"{_:d}" for _ in value) + "}"
+    if isinstance(value[0], list):
+        return "{" + ", ".join(brac(_) for _ in value) + "}"
 
 
 def write_MTP(file, data: dict[str, Any]) -> None:
@@ -85,10 +115,10 @@ def write_MTP(file, data: dict[str, Any]) -> None:
                 j += 1
         f.write(f"alpha_moments_count = {alpha_moments_count}\n")
         f.write(f"alpha_index_basic_count = {alpha_index_basic_count}\n")
-        f.write(f"alpha_index_basic = {alpha_index_basic}\n")
+        f.write(f"alpha_index_basic = {brac(alpha_index_basic)}\n")
         f.write(f"alpha_index_times_count = {alpha_index_times_count}\n")
-        f.write(f"alpha_index_times = {alpha_index_times}\n")
+        f.write(f"alpha_index_times = {brac(alpha_index_times)}\n")
         f.write(f"alpha_scalar_moments = {alpha_scalar_moments}\n")
-        f.write(f"alpha_moment_mapping = {alpha_moment_mapping}\n")
+        f.write(f"alpha_moment_mapping = {brac(alpha_moment_mapping)}\n")
         f.write("species_coeffs = {}\n".format(brac(data["species_coeffs"])))
         f.write("moment_coeffs = {}\n".format(brac(data["moment_coeffs"])))
