c17065eaf8223fa44c3543a1f3558b90b32e9f32
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2024-09-05 00:57:37 +0200

    Reduce `mytarget` calls in `motep/opt.py`

--- motep/opt.py
+++ motep/opt.py
@@ -1,21 +1,25 @@
-from scipy.optimize import differential_evolution, dual_annealing, minimize
+from scipy.optimize import (
+    OptimizeResult,
+    differential_evolution,
+    dual_annealing,
+    minimize,
+)
+
+
+def callback(intermediate_result: OptimizeResult):
+    """A callable called after each iteration."""
+    print("Function value:", intermediate_result.fun)
 
 
 def optimization_sa(mytarget, initial_guess, bounds, *args):
-    def callback_print(params, f, context):
-        func_value = mytarget(params, *args)
-        print("Function value:", func_value)
-        # print("Parameters:", params)
-
     result = dual_annealing(
         mytarget,
         bounds=bounds,
         args=args,
-        callback=callback_print,
+        callback=callback,
         seed=40,
         x0=initial_guess,
     )
-
     print("Optimization result:")
     print("  Success:", result.success)
     print("  Message:", result.message)
@@ -23,16 +27,10 @@ def optimization_sa(mytarget, initial_guess, bounds, *args):
     print("  Number of function evaluations:", result.nfev)
     print("  Final parameters:", result.x)
     print("  Final function value:", result.fun)
-
     return result.x
 
 
 def optimization_nelder(mytarget, initial_guess, bounds, *args):
-    def callback_print(params):
-        func_value = mytarget(params, *args)
-        print("Function value:", func_value)
-        # print("Parameters:", params)
-
     result = minimize(
         mytarget,
         initial_guess,
@@ -40,7 +38,7 @@ def optimization_nelder(mytarget, initial_guess, bounds, *args):
         bounds=bounds,
         method="Nelder-Mead",
         tol=1e-7,
-        callback=callback_print,
+        callback=callback,
         options={"maxiter": 100000},
     )
     print("Optimization result:", result.message)
@@ -56,11 +54,6 @@ def optimization_nelder(mytarget, initial_guess, bounds, *args):
 
 
 def optimization_bfgs(mytarget, initial_guess, bounds, *args):
-    def callback_print(params):
-        func_value = mytarget(params, *args)
-        print("Function value:", func_value)
-        # print("Parameters:", params)
-
     result = minimize(
         mytarget,
         initial_guess,
@@ -68,7 +61,7 @@ def optimization_bfgs(mytarget, initial_guess, bounds, *args):
         bounds=bounds,
         method="L-BFGS-B",
         tol=1e-7,
-        callback=callback_print,
+        callback=callback,
     )
     print("Optimization result:", result.message)
     print("Optimization result:")
@@ -83,14 +76,13 @@ def optimization_bfgs(mytarget, initial_guess, bounds, *args):
 
 
 def optimization_DE(mytarget, initial_guess, bounds, *args):
-    def callback_print(xk, convergence):
-        print("Objective function values:", mytarget(xk, *args))
-        # You can add more details about the optimization process here
-
     result = differential_evolution(
-        mytarget, bounds, args=args, popsize=30, callback=callback_print
+        mytarget,
+        bounds,
+        args=args,
+        popsize=30,
+        callback=callback,
     )
-
     print("Optimization result:")
     print("  Message:", result.message)
     print("  Success:", result.success)
