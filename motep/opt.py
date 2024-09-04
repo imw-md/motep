971d2e2547a6a0eb1743c4d87cef3766aca40f19
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2024-09-05 01:00:48 +0200

    Unify `print_result`

--- motep/opt.py
+++ motep/opt.py
@@ -11,6 +11,18 @@ def callback(intermediate_result: OptimizeResult):
     print("Function value:", intermediate_result.fun)
 
 
+def print_result(result: OptimizeResult):
+    """Print `result`."""
+    print("Optimization result:")
+    print("  Message:", result.message)
+    print("  Success:", result.success)
+    print("  Status code:", result.status)
+    print("  Number of function evaluations:", result.nfev)
+    print("  Number of iterations:", result.nit)
+    print("  Final parameters:", result.x)
+    print("  Final function value:", result.fun)
+
+
 def optimization_sa(mytarget, initial_guess, bounds, *args):
     result = dual_annealing(
         mytarget,
@@ -20,13 +32,7 @@ def optimization_sa(mytarget, initial_guess, bounds, *args):
         seed=40,
         x0=initial_guess,
     )
-    print("Optimization result:")
-    print("  Success:", result.success)
-    print("  Message:", result.message)
-    print("  Status code:", result.status)
-    print("  Number of function evaluations:", result.nfev)
-    print("  Final parameters:", result.x)
-    print("  Final function value:", result.fun)
+    print_result(result)
     return result.x
 
 
@@ -41,15 +47,7 @@ def optimization_nelder(mytarget, initial_guess, bounds, *args):
         callback=callback,
         options={"maxiter": 100000},
     )
-    print("Optimization result:", result.message)
-    print("Optimization result:")
-    print("  Message:", result.message)
-    print("  Success:", result.success)
-    print("  Status code:", result.status)
-    print("  Number of function evaluations:", result.nfev)
-    print("  Number of iterations:", result.nit)
-    print("  Final parameters:", result.x)
-    print("  Final function value:", result.fun)
+    print_result(result)
     return result.x
 
 
@@ -63,15 +61,7 @@ def optimization_bfgs(mytarget, initial_guess, bounds, *args):
         tol=1e-7,
         callback=callback,
     )
-    print("Optimization result:", result.message)
-    print("Optimization result:")
-    print("  Message:", result.message)
-    print("  Success:", result.success)
-    print("  Status code:", result.status)
-    print("  Number of function evaluations:", result.nfev)
-    print("  Number of iterations:", result.nit)
-    print("  Final parameters:", result.x)
-    print("  Final function value:", result.fun)
+    print_result(result)
     return result.x
 
 
@@ -83,10 +73,5 @@ def optimization_DE(mytarget, initial_guess, bounds, *args):
         popsize=30,
         callback=callback,
     )
-    print("Optimization result:")
-    print("  Message:", result.message)
-    print("  Success:", result.success)
-    print("  Number of function evaluations:", result.nfev)
-    print("  Final parameters:", result.x)
-    print("  Final function value:", result.fun)
+    print_result(result)
     return result.x
