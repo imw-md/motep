1582ced9db26c7822c3c2bbd4bcb93f31245901e
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2024-09-25 12:35:14 +0200

    Numpyfy `read_mtp`

--- tests/io/test_mtp.py
+++ tests/io/test_mtp.py
@@ -2,13 +2,19 @@
 
 import pathlib
 
+import numpy as np
+
 from motep.io.mlip.mtp import read_mtp, write_mtp
 
 
-def test_mtp(data_path: pathlib.Path, tmp_path: pathlib.Path):
+def test_mtp(data_path: pathlib.Path, tmp_path: pathlib.Path) -> None:
     """Test consistency between `read_mtp` and `write_mtp`."""
-    path = data_path / f"fitting/molecules/291/02"
+    path = data_path / "fitting/molecules/291/02"
     d_ref = read_mtp(path / "initial.mtp")
     write_mtp(tmp_path / "final.mtp", d_ref)
     d = read_mtp(tmp_path / "final.mtp")
-    assert d == d_ref
+    for key, value in d_ref.items():
+        if isinstance(value, np.ndarray):
+            np.testing.assert_array_equal(d[key], value)
+        else:
+            assert d[key] == value
