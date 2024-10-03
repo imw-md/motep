836bf173537276dc8e9c28bfc14a8ff1e454e70e
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2024-10-03 08:24:18 +0200

    Add `test_lls.py`

---
+++ tests/optimizers/test_lls.py
@@ -0,0 +1,53 @@
+"""Tests for `LLSOptimizer`."""
+
+import pathlib
+
+import pytest
+from mpi4py import MPI
+
+from motep.initializer import Initializer
+from motep.io.mlip.cfg import _get_species, read_cfg
+from motep.io.mlip.mtp import read_mtp
+from motep.loss_function import LossFunction
+from motep.optimizers.lls import LLSOptimizer
+
+
+@pytest.mark.parametrize("level", [2, 4, 6, 8, 10])
+@pytest.mark.parametrize(
+    ("molecule", "species"),
+    [(762, {1: 0}), (291, {6: 0, 1: 1}), (14214, {9: 0, 1: 1}), (23208, {8: 0})],
+)
+@pytest.mark.parametrize("engine", ["numpy"])
+def test_molecules(
+    engine: str,
+    molecule: int,
+    species: dict[int, int],
+    level: int,
+    data_path: pathlib.Path,
+) -> None:
+    """Test `LLSOptimizer` for molecules."""
+    original_path = data_path / f"original/molecules/{molecule}"
+    fitting_path = data_path / f"fitting/molecules/{molecule}/{level:02d}"
+    if not (fitting_path / "initial.mtp").exists():
+        pytest.skip()
+    data = read_mtp(fitting_path / "initial.mtp")
+    images = read_cfg(original_path / "training.cfg", index=":")
+
+    species = list(_get_species(images))
+
+    setting = {
+        "energy-weight": 1.0,
+        "force-weight": 0.01,
+        "stress-weight": 0.0,
+    }
+
+    initializer = Initializer(images, species, rng=42)
+    parameters_ref, bounds = initializer.initialize(data, optimized=["moment_coeffs"])
+    loss_function = LossFunction(
+        images,
+        untrained_mtp=fitting_path / "initial.mtp",
+        setting=setting,
+        comm=MPI.COMM_WORLD,
+        engine=engine,
+    )
+    LLSOptimizer(data)(loss_function, parameters_ref, bounds)
