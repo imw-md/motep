09c43fd5eeebbf1a3430941ce669eea574e71ccb
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2024-10-05 11:59:33 +0200

    Use `self.loss_function`

--- motep/optimizers/lls.py
+++ motep/optimizers/lls.py
@@ -3,7 +3,6 @@
 import numpy as np
 from ase import Atoms
 
-from motep.loss_function import LossFunction
 from motep.optimizers import OptimizerBase
 from motep.optimizers.scipy import Callback
 
@@ -44,8 +43,8 @@ class LLSOptimizer(OptimizerBase):
         # Update self.data based on the initialized parameters
         self.loss_function.mtp_data.update(parameters)
 
-        matrix = self._calc_matrix(self.loss_function)
-        vector = self._calc_vector(self.loss_function)
+        matrix = self._calc_matrix()
+        vector = self._calc_vector()
 
         # TODO: Consider also forces and stresses
         moment_coeffs = np.linalg.lstsq(matrix, vector, rcond=None)[0]
@@ -58,39 +57,41 @@ class LLSOptimizer(OptimizerBase):
 
         return parameters
 
-    def _calc_matrix(self, fitness: LossFunction) -> np.ndarray:
+    def _calc_matrix(self) -> np.ndarray:
         """Calculate the matrix for linear least squares (LLS)."""
-        dict_mtp = self.loss_function.mtp_data.dict_mtp
-        images = fitness.images
+        loss_function = self.loss_function
+        dict_mtp = loss_function.mtp_data.dict_mtp
+        images = loss_function.images
+        setting = loss_function.setting
         basis_values = np.array([atoms.calc.engine.basis_values for atoms in images])
         basis_derivs = np.vstack([atoms.calc.engine.basis_derivs.T for atoms in images])
         basis_derivs = basis_derivs.reshape((-1, dict_mtp["alpha_scalar_moments"]))
         tmp = (
-            np.sqrt(fitness.setting["energy-weight"]) * basis_values,
-            np.sqrt(fitness.setting["force-weight"]) * basis_derivs,
+            np.sqrt(setting["energy-weight"]) * basis_values,
+            np.sqrt(setting["force-weight"]) * basis_derivs,
         )
         return np.vstack(tmp)
 
-    def _calc_vector(self, fitness: LossFunction) -> np.ndarray:
+    def _calc_vector(self) -> np.ndarray:
         """Calculate the vector for linear least squares (LLS)."""
-        images = fitness.images
-        energies = self._calc_interaction_energies(fitness)
+        loss_function = self.loss_function
+        images = loss_function.images
+        setting = loss_function.setting
+        energies = self._calc_interaction_energies()
         forces = np.hstack(
-            [fitness.target_forces[i].reshape(-1) for i, atoms in enumerate(images)]
+            [
+                loss_function.target_forces[i].reshape(-1)
+                for i, atoms in enumerate(images)
+            ],
         )
         tmp = (
-            np.sqrt(fitness.setting["energy-weight"]) * energies,
-            np.sqrt(fitness.setting["force-weight"]) * forces * -1.0,
+            np.sqrt(setting["energy-weight"]) * energies,
+            np.sqrt(setting["force-weight"]) * forces * -1.0,
         )
         return np.hstack(tmp)
 
-    def _calc_interaction_energies(self, fitness: LossFunction) -> np.ndarray:
+    def _calc_interaction_energies(self) -> np.ndarray:
         """Calculate interaction energies of Atoms objects.
-
-        Parameters
-        ----------
-        fitness : :class:`~motep.loss_function.LossFunction`
-            :class:`motep.loss_function.LossFunction` object.
 
         Returns
         -------
@@ -99,15 +100,16 @@ class LLSOptimizer(OptimizerBase):
             interactions among atoms, i.e., without site energies.
 
         """
-        dict_mtp = self.loss_function.mtp_data.dict_mtp
+        loss_function = self.loss_function
+        dict_mtp = loss_function.mtp_data.dict_mtp
+        images = loss_function.images
         species = dict_mtp["species"]
-        images = fitness.images
 
         def get_types(atoms: Atoms) -> list[int]:
             return [species[_] for _ in atoms.numbers]
 
         iterable = (
-            fitness.target_energies[i]
+            loss_function.target_energies[i]
             - np.add.reduce(dict_mtp["species_coeffs"][get_types(atoms)])
             for i, atoms in enumerate(images)
         )
