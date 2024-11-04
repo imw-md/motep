6cd9f8527022404916284819193a98d6b3f686a6
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2024-11-04 22:13:04 +0100

    Add `test_alpha_index_times`

--- tests/io/test_mtp.py
+++ tests/io/test_mtp.py
@@ -3,6 +3,7 @@
 import pathlib
 
 import numpy as np
+import pytest
 
 from motep.io.mlip.mtp import read_mtp, write_mtp
 
@@ -18,3 +19,12 @@ def test_mtp(data_path: pathlib.Path, tmp_path: pathlib.Path) -> None:
             np.testing.assert_array_equal(d[key], value)
         else:
             assert d[key] == value
+
+
+@pytest.mark.parametrize("level", [2, 4])
+def test_alpha_index_times(level: int, data_path: pathlib.Path) -> None:
+    """Test shape of `alpha_index_times`."""
+    path = data_path / f"fitting/molecules/291/{level:02d}"
+    mtp_data = read_mtp(path / "initial.mtp")
+    assert mtp_data["alpha_index_times"].ndim == 2
+    assert mtp_data["alpha_index_times"].shape[-1] == 4
