7c0e4943f084f609f2a2151c20581f08154e6b05
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2025-02-23 22:20:44 +0100

    Convert more keys to lists

--- motep/setting.py
+++ motep/setting.py
@@ -68,8 +68,10 @@ def parse_setting(filename: str) -> Setting:
     with pathlib.Path(filename).open("rb") as f:
         setting_overwritten = tomllib.load(f)
 
-    if isinstance(setting_overwritten["configurations"], str):
-        setting_overwritten["configurations"] = [setting_overwritten["configurations"]]
+    keys = ["configurations", "data_training", "data_in", "data_out"]
+    for key in keys:
+        if key in setting_overwritten and isinstance(setting_overwritten[key], str):
+            setting_overwritten[key] = [setting_overwritten[key]]
 
     # convert the old style "steps" like {'steps`: ['L-BFGS-B']} to the new one
     # {'steps`: {'method': 'L-BFGS-B'}
