2e4d76b7d3a8863b41853df12096cbd444480499
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2026-03-19 15:25:38 +0100

    Add `test_grader.py`

---
+++ tests/grader/test_grader.py
@@ -0,0 +1,28 @@
+"""Tests for active-learning algorithms."""
+
+from pathlib import Path
+
+import numpy as np
+import pytest
+
+from motep.grader.grader import GradeMode, Grader
+from motep.io.mlip.cfg import read_cfg
+from motep.io.mlip.mtp import read_mtp
+
+
+@pytest.mark.parametrize("mode", list(GradeMode))
+def test_grader(mode: GradeMode, data_path: Path) -> None:
+    """Test if `Grader` works."""
+    path = data_path / "fitting/crystals/multi/10"
+    images_training = read_cfg(path / "out.cfg", index=":")
+    mtp_data = read_mtp(path / "pot.mtp")
+
+    grades_ref = [atoms.info["MV_grade"] for atoms in images_training]
+
+    grader = Grader(mtp_data, mode=mode)
+    grader.update(images_training)
+    grader.grade(images_training)
+
+    grades = [atoms.info["MV_grade"] for atoms in images_training]
+
+    # np.testing.assert_allclose(grades, grades_ref)  # TODO
