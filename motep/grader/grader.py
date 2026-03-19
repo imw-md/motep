bd0aaa8cf8e339f8089401cb15b03025577a1f43
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2026-03-19 13:41:48 +0100

    Add option to choose initial indices for `MaxVol`

--- motep/grader/grader.py
+++ motep/grader/grader.py
@@ -2,6 +2,7 @@
 
 import logging
 import pathlib
+from dataclasses import dataclass, field
 from pprint import pformat
 
 import numpy as np
@@ -9,14 +10,37 @@ from ase import Atoms
 
 import motep.io
 from motep.calculator import MTP
-from motep.grader.algorithms import find_active_set_exhaustive, find_active_set_maxvol
+from motep.grader.maxvol import MaxVol, MaxVolSetting
 from motep.io.mlip.mtp import read_mtp
 from motep.io.utils import get_dummy_species, read_images
 from motep.parallel import DummyMPIComm, world
 from motep.potentials.mtp.data import MTPData
-from motep.setting import load_setting_grade
+from motep.setting import Setting, parse_setting
 
 logger = logging.getLogger(__name__)
+
+
+@dataclass
+class GradeSetting(Setting):
+    """Setting for the extrapolation-grade calculations."""
+
+    mode: str = "configuration"
+    maxvol: MaxVolSetting = field(default_factory=MaxVolSetting)
+
+    def __post_init__(self) -> None:
+        """Postprocess."""
+        self.maxvol = MaxVolSetting.from_any(self.maxvol)
+
+
+def load_setting_grade(filename: str) -> GradeSetting:
+    """Load setting for `grade`.
+
+    Returns
+    -------
+    GradeSetting
+
+    """
+    return GradeSetting(**parse_setting(filename))
 
 
 class Grader:
@@ -30,11 +54,12 @@ class Grader:
         engine: str = "cext",
         *,
         mode: str = "configuration",
-        algorithm: str = "maxvol",
-        maxiter: int = 100_000,
+        maxvol_setting: MaxVolSetting | dict | None = None,
         comm: DummyMPIComm = world,
     ) -> None:
         """Initialize."""
+        maxvol_setting = MaxVolSetting.from_any(maxvol_setting)
+
         self.mtp_data = mtp_data
 
         seed = seed or comm.bcast(np.random.SeedSequence().entropy % (2**32), root=0)
@@ -43,9 +68,8 @@ class Grader:
         self.rng = rng or np.random.default_rng(seed)
 
         self.engine = engine
-        self.algorithm = algorithm
+        self.maxvol_setting = maxvol_setting
         self.mode = mode
-        self.maxiter = maxiter
         self.comm = comm
         self.indices = np.array(np.iinfo(np.int32).min, dtype=int)
         self.active_set_matrix = np.array(np.nan)
@@ -58,22 +82,18 @@ class Grader:
         images : list[Atoms]
             List of ASE Atoms objects used for training.
 
-        Raises
-        ------
-        ValueError
-
         """
         matrix = self.calc_matrix(images)
-        if self.algorithm == "exhaustive":
-            self.indices = find_active_set_exhaustive(matrix)
-        elif self.algorithm == "maxvol":
-            self.indices = find_active_set_maxvol(
-                matrix,
-                self.rng,
-                maxiter=self.maxiter,
-            )
-        else:
-            raise ValueError(self.algorithm)
+        maxvol = MaxVol(
+            algorithm=self.maxvol_setting.algorithm,
+            init_method=self.maxvol_setting.init_method,
+            rng=self.rng,
+        )
+        self.indices = maxvol.run(
+            matrix,
+            threshold=self.maxvol_setting.threshold,
+            maxiter=self.maxvol_setting.maxiter,
+        )
         self.active_set_matrix = matrix[self.indices]
 
     def calc_matrix(self, images: list[Atoms]) -> np.ndarray:
@@ -174,8 +194,7 @@ def grade(filename_setting: str, comm: DummyMPIComm = world) -> None:
         engine=setting.engine,
         rng=rng,
         mode=setting.mode,
-        algorithm=setting.algorithm,
-        maxiter=setting.maxiter,
+        maxvol_setting=MaxVolSetting.from_any(setting.maxvol),
         comm=comm,
     )
     grader.update(images_training)
---
+++ motep/grader/maxvol.py
@@ -0,0 +1,224 @@
+"""Module for MaxVol algorithms."""
+
+import logging
+from collections.abc import Callable
+from dataclasses import dataclass, field
+from enum import StrEnum
+from itertools import combinations
+from math import comb
+
+import numpy as np
+from scipy.linalg import qr
+
+from motep.setting import DataclassFromAny
+
+logger = logging.getLogger(__name__)
+
+
+def _init_maxvol_first(matrix: np.ndarray) -> np.ndarray:
+    return np.arange(matrix.shape[1])
+
+
+def _init_maxvol_last(matrix: np.ndarray) -> np.ndarray:
+    return np.arange(matrix.shape[0] - matrix.shape[1], matrix.shape[0])
+
+
+def _init_maxvol_qr(matrix: np.ndarray) -> np.ndarray:
+    return qr(matrix.T, pivoting=True)[-1][: matrix.shape[1]]
+
+
+def _init_maxvol_random(matrix: np.ndarray, *, rng: np.random.Generator) -> np.ndarray:
+    return rng.choice(matrix.shape[0], matrix.shape[1], replace=False)
+
+
+class InitMethod(StrEnum):
+    """Initialization method for the indices for the MaxVol calculation."""
+
+    FIRST = "first"
+    LAST = "last"
+    QR = "qr"
+    RANDOM = "random"
+
+
+_INIT_METHODS: dict[InitMethod, Callable] = {
+    InitMethod.FIRST: _init_maxvol_first,
+    InitMethod.LAST: _init_maxvol_last,
+    InitMethod.RANDOM: _init_maxvol_random,
+    InitMethod.QR: _init_maxvol_qr,
+}
+
+
+def _validate_matrix(matrix: np.ndarray) -> None:
+    if matrix.ndim != 2:
+        msg = "matrix must be 2-dimensional"
+        raise ValueError(msg)
+    nrows, ncols = matrix.shape
+    if nrows < ncols:
+        msg = "matrix must satisfy nrows >= ncols"
+        raise ValueError(msg)
+
+
+def _validate_indices(matrix: np.ndarray, indices: np.ndarray) -> None:
+    if indices.ndim != 1:
+        msg = "indices must be 1-dimensional"
+        raise ValueError(msg)
+    nrows, ncols = matrix.shape
+    if indices.size != ncols:
+        msg = "indices length must be ncols"
+        raise ValueError(msg)
+    if len(np.unique(indices)) != indices.size:
+        msg = "indices must be unique"
+        raise ValueError(msg)
+    if np.any(indices < 0) or np.any(indices >= nrows):
+        msg = "indices must be 0 <= indices < nrows"
+        raise ValueError(msg)
+
+
+def _exhaust(matrix: np.ndarray) -> np.ndarray:
+    """Find the MaxVol indices exhaustively.
+
+    Returns
+    -------
+    indices : np.ndarray
+        MaxVol indices.
+
+    Raises
+    ------
+    RuntimeError
+
+    """
+    _validate_matrix(matrix)
+    nrows, ncols = matrix.shape
+
+    # Choose rows (configurations)
+    # This is preliminarily implemented only in an exhausive manner.
+    # This is therefore valid so far only for a small `data_in`
+    # and for a low level `potential_final`.
+    if comb(nrows, ncols) > 2**24:  # 16777216
+        msg = "too large possible combinations of rows"
+        raise RuntimeError(msg, comb(nrows, ncols))
+    slogdet_max = -np.inf
+    indices = np.arange(ncols)
+    for _ in combinations(range(nrows), ncols):
+        indices_checked = np.array(_, dtype=int)
+        submatrix = matrix[indices_checked]
+        sign, slogdet = np.linalg.slogdet(submatrix)  # for numerical stability
+        if sign == 0.0:
+            continue
+        if slogdet > slogdet_max:
+            indices = indices_checked
+            slogdet_max = slogdet
+
+    return indices
+
+
+def _maxvol(
+    matrix: np.ndarray,
+    indices: np.ndarray,
+    *,
+    threshold: float = 1e-9,
+    maxiter: int = 100_100,
+) -> np.ndarray:
+    """Find the MaxVol indices.
+
+    Returns
+    -------
+    indices : np.ndarray
+        MaxVol indices.
+
+    """
+    _validate_matrix(matrix)
+    _validate_indices(matrix, indices)
+    nrows, ncols = matrix.shape
+    flags = np.zeros(nrows, dtype=bool)
+    flags[indices] = True
+
+    for _ in range(maxiter):
+        submatrix = matrix[flags]
+        c = np.linalg.lstsq(submatrix.T, matrix.T, rcond=None)[0].T
+        i, j = np.divmod(np.argmax(np.abs(c)), ncols)
+        if np.abs(c[i, j]) - 1.0 < threshold:
+            break
+        selected = np.flatnonzero(flags)
+        k = selected[j]
+        flags[k] = False
+        flags[i] = True
+    else:
+        cmax = np.abs(c[i, j])
+        msg = (
+            f"Maxvol algorithm did not converge within {maxiter} iterations. "
+            f"Current c-max: {cmax}"
+        )
+        logger.warning(msg)
+
+    return np.flatnonzero(flags)
+
+
+class FindMethod(StrEnum):
+    """Finding method for the indices for he MaxVol calculation."""
+
+    EXHAUST = "exhaust"
+    MAXVOL = "maxvol"
+
+
+@dataclass
+class MaxVolSetting(DataclassFromAny):
+    """MaxVol setting."""
+
+    algorithm: FindMethod = FindMethod.MAXVOL
+    init_method: InitMethod = InitMethod.QR
+    threshold: float = 1e-9
+    maxiter: int = 100_000
+
+
+@dataclass
+class MaxVol:
+    """MaxVol algorithm."""
+
+    algorithm: FindMethod = FindMethod.MAXVOL
+    init_method: InitMethod = InitMethod.QR
+    rng: np.random.Generator | None = None
+    init_fn: Callable[..., np.ndarray] = field(init=False)
+
+    def __post_init__(self) -> None:
+        """Set up the initialization method.
+
+        Raises
+        ------
+        ValueError
+
+        """
+        try:
+            self.init_fn = _INIT_METHODS[self.init_method]
+        except KeyError as err:
+            msg = f"Unknown init method: {self.init_method}"
+            raise ValueError(msg) from err
+
+    def run(
+        self,
+        matrix: np.ndarray,
+        *,
+        threshold: float = 1e-9,
+        maxiter: int = 100_100,
+    ) -> np.ndarray:
+        """Find the indices for the MaxVol calculation.
+
+        Returns
+        -------
+        np.ndarray
+
+        Raises
+        ------
+        ValueError
+
+        """
+        _validate_matrix(matrix)
+        if self.algorithm == FindMethod.EXHAUST:
+            return _exhaust(matrix)
+        if self.algorithm == FindMethod.MAXVOL:
+            if self.init_method == InitMethod.RANDOM:
+                indices = self.init_fn(matrix, rng=self.rng)
+            else:
+                indices = self.init_fn(matrix)
+            return _maxvol(matrix, indices, threshold=threshold, maxiter=maxiter)
+        raise ValueError(self.algorithm)
