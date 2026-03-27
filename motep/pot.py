87c4afb2c5c8fb2e7e6549b9e6780192c2a11f10
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2026-03-27 21:07:19 +0100

    Remove `pot.py`

--- motep/pot.py
+++
@@ -1,9 +0,0 @@
-from ase.calculators.morse import MorsePotential
-
-
-def Morse_field(parameters):
-    epsilon, rho0, r0, rcut1, rcut2 = parameters
-    Potential = MorsePotential(
-        epsilon=epsilon, rho0=rho0, r0=r0, rcut1=rcut1, rcut2=rcut2
-    )
-    return Potential
