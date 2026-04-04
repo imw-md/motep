9dab2f1377d45c12297f3efcc1d1a7b70346a36d
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2026-04-04 11:25:48 +0200

    Add `test_comparision_with_mlip`

--- tests/grade/test_grader.py
+++ tests/grade/test_grader.py
@@ -25,12 +25,26 @@ def test_grader(optimized: list[str], mode: GradeMode, data_path: Path) -> None:
     mtp_data = read_mtp(path / "pot.mtp")
     mtp_data.optimized = optimized
 
+    grader = Grader(mtp_data, mode=mode)
+    grader.update(images_training)
+    images_out = grader.grade(images_training)
+
+    assert all("MV_grade" in atoms.calc.results for atoms in images_out)
+
+
+def test_comparision_with_mlip(data_path: Path) -> None:
+    """Test if `Grader` gives the extrapolation grades consistent with MLIP."""
+    path = data_path / "fitting/crystals/multi/10"
+    images_training = read_cfg(path / "out.cfg", index=":")
+    mtp_data = read_mtp(path / "pot.mtp")
+    mtp_data.optimized = ["moment_coeffs", "species_coeffs", "radial_coeffs"]
+
     grades_ref = [atoms.calc.results["MV_grade"] for atoms in images_training]
 
-    grader = Grader(mtp_data, mode=mode)
+    grader = Grader(mtp_data, mode="neighborhood", maxvol_setting={"algorithm": "mlip"})
     grader.update(images_training)
     images_out = grader.grade(images_training)
 
     grades = [atoms.calc.results["MV_grade"] for atoms in images_out]
 
-    # np.testing.assert_allclose(grades, grades_ref)  # TODO
+    np.testing.assert_allclose(grades, grades_ref, rtol=1e-6, atol=1e-6)
